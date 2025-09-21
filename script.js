7e81f789c7375513a4e420382d239a2fef006154
Mark Emmanuel <dmarksman1911@gmail.com>
2025-09-20 16:24:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 document.getElementById("voteForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const selectedVotes = {};
   const form = e.target;
-  const inputs = form.querySelectorAll("input[type='radio']:checked");
+  const formData = new FormData(form);
 
-  inputs.forEach(input => {
-    selectedVotes[input.name] = input.value;
-  });
-
-  fetch("https://script.google.com/macros/s/AKfycby5D44vIceI0LtxCmaUMXuW_UIaeJNbVCaZjXb8iNHirPHz3td8eUm__wPmbSsS7GoN/exec", {
+  fetch("YOUR_WEB_APP_URL", {
     method: "POST",
-    body: JSON.stringify(selectedVotes),
-    headers: {
-      "Content-Type": "application/json"
-    }
+    body: formData
   })
   .then(response => response.text())
   .then(result => {
-    console.log("Submitted:", result);
+    console.log("Vote Submitted:", result);
     form.style.display = "none";
     document.getElementById("confirmationMessage").style.display = "block";
   })
